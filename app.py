a20b2ca7942f787f28dbc90c184089f43c4eec85
Dario <44131842+apokrashenko@users.noreply.github.com>
2022-05-31 01:57:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,6 @@ def main():
 
     if page == "Homepage":
         st.header("Данные для задачи классификации заголовков.")
-        st.write("Данные для датасета были извлечены из изображений, снятых с подлинных и поддельных банкнотоподобных образцов. Для оцифровки использовалась промышленная камера, обычно используемая для проверки печати.Инструмент Wavelet Transform использовался для извлечения признаков из изображений.")
-        st.write("1) variance of Wavelet Transformed image (дисперсия вейвлет-преобразованного изображения), тип вещественный.")
-        st.write("2) skewness of Wavelet Transformed image (асимметрия вейвлет-преобразованного изображения), тип вещественный.")
-        st.write("3) curtosis of Wavelet Transformed image (эксцесс преобразованного изображения), тип вещественный.")
-        st.write("4) entropy of image (энтропия изображения), тип вещественный.")
-        st.write(df)
     elif page == "Exploration":
         st.title("Data Exploration")
         visualize_data(df)
@@ -29,6 +23,7 @@ def main():
 def load_data():
     df = pd.read_csv('train_data_preprocessed.tsv', sep='\t')
     return df
+
 
 def visualize_data(df):
     c = alt.Chart(df).mark_circle().encode(x='variance', y='skewness',
