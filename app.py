d71005090be395b0eeb24cd35efe211935e7f6a6
Dario <44131842+apokrashenko@users.noreply.github.com>
2022-05-31 01:49:52 +0600

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import SGDClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report, confusion_matrix
 import streamlit as st
 import altair as alt
 import pandas as pd
-import numpy as np
-from sklearn.tree import DecisionTreeClassifier
 import pickle
 
 def main():
